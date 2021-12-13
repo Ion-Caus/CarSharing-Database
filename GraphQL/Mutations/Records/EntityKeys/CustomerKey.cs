86e2e0c21304e45aebd1477f392ae42069c39fe3
Ion-Caus <causion01@gmail.com>
2021-12-13 14:31:34 +0100

    split the record in different files

--- GraphQL/Mutations/Records/EntityKeys/CustomerKey.cs
+++ GraphQL/Mutations/Records/EntityKeys/CustomerKey.cs
@@ -1,8 +1,4 @@
 namespace CarSharing_Database_GraphQL.Mutations.Records
 {
-    public record VehicleKey(string LicenseNo);
-    
     public record CustomerKey(string Cpr);
-    
-    public record ListingKey(int Id);
 }
